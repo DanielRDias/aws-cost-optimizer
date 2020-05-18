7095bd32833485237da46fb89fd09457604c0dbd
Daniel Dias <daniel.dias@continental-corporation.com>
2020-05-18 21:40:56 +0200

    fix(awsoptimizer): fixed account id, in case it starts with 0

--- cloudmanager/awsoptimizer/models.py
+++ cloudmanager/awsoptimizer/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class AwsOptimizer(models.Model):
     name = models.CharField(max_length=64)
     email = models.EmailField(max_length=64, unique=True)
-    accountid = models.IntegerField(unique=True)
+    # Account ID can start with 0
+    accountid = models.CharField(max_length=12)
     account_name = models.CharField(max_length=64)
     message = models.CharField(max_length=500, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
